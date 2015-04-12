5d1b6067c259c9fa11ac7c13ee39802eff619488
Vijay Subramaniyam <vijay.subramaniyam@nowfloats.com>
2015-04-12 13:16:34 +0530

    Initial Commit

---
+++ VSCardView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VSCardView
+//
+//  Created by Vijay subramaniyam on 12/04/15.
+//  Copyright (c) 2015 vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VSCardView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VSCardView
+//
+//  Created by Vijay subramaniyam on 12/04/15.
+//  Copyright (c) 2015 vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
